511c60e720dbc303dbb03ec75a3743bafd62b612
asvetek <ales.svetek@cern.ch>
2018-08-10 06:14:26 -0500

    Ruckus based build ZYNQ test firmware

---
+++ firmware/ruckus.tcl
@@ -0,0 +1,14 @@
+# Load RUCKUS environment and library
+ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+ set_property ip_repo_paths $::env(XIL_IPI_REPO) [current_project]
+ update_ip_catalog
+
+# # Load common and sub-module ruckus.tcl files
+#loadRuckusTcl $::env(PROJ_DIR)/../../../
+#loadRuckusTcl $::env(PROJ_DIR)
+#
+# # Load local source Code and constraints
+loadSource      -dir "$::DIR_PATH/hdl/"
+loadBlockDesign -dir "$::DIR_PATH/bd/"
+#loadConstraints -dir "$::DIR_PATH/constraints/"
